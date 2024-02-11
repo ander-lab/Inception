33010ac2dc2d862e4e5446acdedc6969874c847d
ander-lab <jimenezs.alex@icloud.com>
2024-02-11 16:19:00 +0100

    not finished

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -36,39 +36,39 @@
 #cat wp-config.php
 #config_wp_php
 
-while ! mariadb -h"mariadb" -u$DB_USER -p$DB_PASSWORD $DB_NAME --silent; do
-	echo "[INFO] waiting mariadb..."
-	sleep 1;
-done
-	
-
-
-wp core download --allow-root
-
-wp config create --allow-root \
-	--path="/var/www/html/wordpress" --allow-root\
-	--dbname=$MYSQL_NAME \
-	--dbuser=$MYSQL_USER \
-	--dbpass=$MYSQL_PASSWORD \
-	--dbhost=127.0.0.1 \
-	--locale=es_ES
-
-
-wp core install     --path="/var/www/html/wordpress" --allow-root \
-					--title="INCEPTION" \
-					--url=$DOMAIN_NAME \
-					--title="INCEPTION" \
-					--admin_user=$WP_ADMIN_USER \
-					--admin_password=$WP_ADMIN_PASSWORD \
-					--admin_email=$WP_ADMIN_EMAIL
-# wp option home value
-# wp option 
-wp user create      --path="/var/www/html/wordpress" --allow-root \
-					$WP_USER $WP_EMAIL \
-					--user_pass=$WP_USER_PASS \
-					--role=author
-
-
-
+#while ! mariadb -h"mariadb" -u$DB_USER -p$DB_PASSWORD $DB_NAME --silent; do
+#	echo "[INFO] waiting mariadb..."
+#	sleep 1;
+#done
+#	
+#
+#
+#wp core download --allow-root
+#
+#wp config create --allow-root \
+#	--path="/var/www/html/wordpress" --allow-root\
+#	--dbname=$MYSQL_NAME \
+#	--dbuser=$MYSQL_USER \
+#	--dbpass=$MYSQL_PASSWORD \
+#	--dbhost=127.0.0.1 \
+#	--locale=es_ES
+#
+#
+#wp core install     --path="/var/www/html/wordpress" --allow-root \
+#					--title="INCEPTION" \
+#					--url=$DOMAIN_NAME \
+#					--title="INCEPTION" \
+#					--admin_user=$WP_ADMIN_USER \
+#					--admin_password=$WP_ADMIN_PASSWORD \
+#					--admin_email=$WP_ADMIN_EMAIL
+## wp option home value
+## wp option 
+#wp user create      --path="/var/www/html/wordpress" --allow-root \
+#					$WP_USER $WP_EMAIL \
+#					--user_pass=$WP_USER_PASS \
+#					--role=author
+#
+#
+#
 mkdir -p /run/php/
 php-fpm7.4 --nodaemonize
