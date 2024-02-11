0f933901cc0df0681ca7df6504533a9c9e8546b6
ander-lab <jimenezs.alex@icloud.com>
2024-02-11 18:53:21 +0100

    wp

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -5,6 +5,9 @@ define( 'DB_PASSWORD', 'password_here' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
 
 define('AUTH_KEY',         'Wp+QMX^nx0 RpAh$~V8d}Eo,y[Eu__co2]gd>-TE@*g^vdo!oK#.vTz$iQ){YIPm');
 define('SECURE_AUTH_KEY',  'Bo@PisyKHj(745KS^.w~sH0W85|F|FNMIOMWv~~cTUp0}1=(hR7xx8l)4@5>4G5E');
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,54 +1,10 @@
 #!/bin/bash
 
+mv /wordpress /var/www/html
 
-if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
-	wp core download --allow-root
-	wp config create --allow-root \
-		--path="/var/www/html/wordpress" --allow-root\
-		--dbname=$DB_NAME \
-		--dbuser=$DB_USER \
-		--dbpass=$DB_PASSWORD \
-		--dbhost=127.0.0.1 \
-		--locale=es_ES
+sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$DB_USER/g" 	 /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/g"  /var/www/html/wordpress/wp-config.php
 
-
-<<<<<<< HEAD
-#cat wp-config.php
-#config_wp_php
-
-#while ! mariadb -h"mariadb" -u$DB_USER -p$DB_PASSWORD $DB_NAME --silent; do
-#	echo "[INFO] waiting mariadb..."
-#	sleep 1;
-#done
-#	
-#
-#
-#wp core download --allow-root
-#
-#wp config create --allow-root \
-#	--path="/var/www/html/wordpress" --allow-root\
-#	--dbname=$MYSQL_NAME \
-#	--dbuser=$MYSQL_USER \
-#	--dbpass=$MYSQL_PASSWORD \
-#	--dbhost=127.0.0.1 \
-#	--locale=es_ES
-#
-#
-#wp core install     --path="/var/www/html/wordpress" --allow-root \
-#					--title="INCEPTION" \
-#					--url=$DOMAIN_NAME \
-#					--title="INCEPTION" \
-#					--admin_user=$WP_ADMIN_USER \
-#					--admin_password=$WP_ADMIN_PASSWORD \
-#					--admin_email=$WP_ADMIN_EMAIL
-## wp option home value
-## wp option 
-#wp user create      --path="/var/www/html/wordpress" --allow-root \
-#					$WP_USER $WP_EMAIL \
-#					--user_pass=$WP_USER_PASS \
-#					--role=author
-#
-#
-#
 mkdir -p /run/php/
 php-fpm7.4 --nodaemonize
