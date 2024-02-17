fde1ccd97c7e1f13b4b6e994282324f93d99b474
ander-lab <jimenezs.alex@icloud.com>
2024-02-17 17:41:26 +0100

    wp done

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -2,14 +2,17 @@
 mkdir -p /var/lib/mysql
 service mariadb start
 sleep 5
-if ! mysql -e "SELECT user FROM mysql.user WHERE user='$DB_USER'" | grep "$DB_USER"; then
-	echo DEBUGGGG
-    mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWD';"
-fi
+
 if ! mysql -e "USE $DB_NAME"; then
     mysql -e "CREATE DATABASE $DB_NAME;"
+fi
+
+if ! mysql -e "SELECT user FROM mysql.user WHERE user='$DB_USER'" | grep "$DB_USER"; then
+	echo DEBUGGGG
+    mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 fi
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 service mariadb stop
 mysqld_safe
+
