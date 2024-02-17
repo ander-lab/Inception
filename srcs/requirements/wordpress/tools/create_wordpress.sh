cb90d75771348acd6fb9c7186a28cfcd3520c7a4
Alex <jimenezs.alex@gmail.com>
2024-02-17 18:57:36 +0100

    inception finished rc1

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -13,6 +13,7 @@ if [ ! -f "/var/www/html/wordpress/wp_configured" ]; then
 	sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wordpress/wp-config.php
 	sed -i "s/username_here/$DB_USER/g" 	 /var/www/html/wordpress/wp-config.php
 	sed -i "s/password_here/$DB_PASSWORD/g"  /var/www/html/wordpress/wp-config.php
+	wp core install --url=localhost --title="ajimenez" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org --allow-root
 	touch wp_configured
 fi
 
